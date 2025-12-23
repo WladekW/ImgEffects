5cbda2e0aacb551d283a82d89017abc9c4bd420b
WladekW <bagdatwl@gmail.com>
2025-12-23 22:57:47 +0100

    ;[

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,20 +1,12 @@
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
