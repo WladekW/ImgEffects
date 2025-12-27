0bb47a81f1ef146fe58b9b4cd490f1cdff3f2b9b
WladekW <bagdatwl@gmail.com>
2025-12-27 22:02:41 +0100

    dither::ordered::bayer::dither_colored 16x16

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,7 @@ use image::{GenericImageView, imageops::FilterType::Nearest};
 mod dither;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let img = image::open("C:/Users/Wlad/Downloads/lampa.jpg")
-        .unwrap()
-        .resize(520, 520, Nearest);
-
+    let img = image::open("C:/Users/Wlad/Downloads/miko.jpg").unwrap();
     let (img_x, img_y) = img.dimensions();
 
     println!("{}:{}", img_x, img_y);
@@ -22,7 +19,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // img_out.save("C:/Users/Wlad/Downloads/clr.png")?;
     // println!("Img clr saved!");
 
-    let img_out = dither::ordered::bayer::dither_duoton(8, &img, [31, 70, 49], [255, 87, 26]);
+    let img_out = dither::ordered::bayer::dither_colored(16, &img);
 
     img_out.save("C:/Users/Wlad/Downloads/duo.webp")?;
     println!("Img duo saved!");
